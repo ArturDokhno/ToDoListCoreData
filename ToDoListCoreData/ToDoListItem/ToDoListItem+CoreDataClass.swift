4711c09cd95f75b509441158595ffd636733ae2c
ArturDokhno <artur.dohno@icloud.com>
2022-02-23 19:57:23 +0500

    Реализация ТоDoList

---
+++ ToDoListCoreData/ToDoListItem/ToDoListItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  ToDoListItem+CoreDataClass.swift
+//  ToDoListCoreData
+//
+//  Created by Артур Дохно on 23.02.2022.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(ToDoListItem)
+public class ToDoListItem: NSManagedObject {
+
+}
